7d83c30418cb4a1d519989f76159843c716a5979
Sara Marcely Andrade de Oliveira <saradomacario@gmail.com>
2025-04-04 21:55:42 -0300

    PAM - CoinFlip 040425

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+
+            /*count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);*/
+        }
+    }
+
+}
